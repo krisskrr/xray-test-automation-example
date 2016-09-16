7172e220272f08a9f463d236c1aa1d637640883b
kristapsmelderis <kmelderis0010@gmail.com>
2016-09-16 16:38:39 +0300

    get test project to runnable state

---
+++ src/test/java/com/kristapsmelderis/examples/stepdefs/search/Google.java
@@ -0,0 +1,31 @@
+package com.kristapsmelderis.examples.stepdefs.search;
+
+import cucumber.api.java.en.*;
+import org.openqa.selenium.By;
+
+import static com.kristapsmelderis.examples.selenium.Driver.driver;
+import static org.junit.Assert.assertTrue;
+
+public class Google {
+    @Given("^I navigate to \"([^\"]*)\"$")
+    public void i_navigate_to(String stepdefInputURL) throws Throwable {
+        driver.get(stepdefInputURL);
+    }
+
+    @When("^I enter \"([^\"]*)\"$")
+    public void i_enter(String stepdefInputString) throws Throwable {
+        driver.findElement(By.id("lst-ib")).sendKeys(stepdefInputString);
+    }
+
+    @When("^I click magnifying glass icon$")
+    public void i_click_button() throws Throwable {
+        driver.findElement(By.className("lsb")).click();
+    }
+
+    @Then("^I can find string \"([^\"]*)\" in page$")
+    public void i_can_find_string_in_page(String stepdefInputString) throws Throwable {
+        String actualPageSource = driver.getPageSource();
+        assertTrue("\n\nAssert Fail: Did not find this string in page source: " + stepdefInputString + "\n\n",
+                actualPageSource.contains(stepdefInputString));
+    }
+}
