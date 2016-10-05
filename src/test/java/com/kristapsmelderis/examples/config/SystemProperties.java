fc28b96108b6ffa8a13c8bd43366627872392f1a
kristapsmelderis <kmelderis0010@gmail.com>
2016-10-05 13:46:54 +0300

    fix key name of a property

--- src/test/java/com/kristapsmelderis/examples/config/SystemProperties.java
+++ src/test/java/com/kristapsmelderis/examples/config/SystemProperties.java
@@ -8,7 +8,6 @@ public class SystemProperties {
     public static String JIRA_PASSWORD = System.getProperty("jiraPassword", "remember_to_set_system_property_for_jiraPassword");
     public static String JIRA_ISSUE_KEYS = System.getProperty("jiraIssueKeys", "remember_to_set_system_property_for_jiraIssueKeys_like_ABC-123;ABC-456");
 
-    public static String IMPORTED_SCENARIO_DIRECTORY = System.getProperty("importedScenarioFilepath", "target/TestsImportedFromJira");
+    public static String IMPORTED_SCENARIO_DIRECTORY = System.getProperty("importedScenarioDirectory", "target/TestsImportedFromJira");
     public static String CUCUMBER_JSON_REPORT_FILEPATH = System.getProperty("cucumberJsonReportFilepath", "target/cucumber.json");
-
 }
